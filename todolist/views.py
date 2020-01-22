fe2b465dac4b0f13dacb4d9b995f4be75e13a113
ttwkr <ttwkor@naver.com>
2020-01-22 18:06:05 +0900

    todo 추가 view API추가

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from .models import TodoList
+
+
+class addTodo(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        TodoList(
+            todo=data['todo']
+        ).save()
+
+        return JsonResponse({'result': 'success'}, status=200)
