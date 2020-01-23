76a8a8b19196dd1b96752b2c8653ca3cb46eaa89
ttwkr <ttwkor@naver.com>
2020-01-23 10:57:49 +0900

    todo checked 기능 구현

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from todolist.views import addTodo, checkedTodo
 
 urlpatterns = [
-    path('', )
+    path('', addTodo.as_view()),
+    path('<int:todo_id>', checkedTodo.as_view())
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -10,7 +10,25 @@ class addTodo(View):
         data = json.loads(request.body)
 
         TodoList(
-            todo=data['todo']
+            todo=data['todo'],
+            check=data['check']
         ).save()
+
+        return JsonResponse({'result': 'success'}, status=200)
+
+    def get(self, request):
+
+        TodoLists = TodoList.objects.all()
+        return JsonResponse({'result': list(TodoLists.values())})
+
+
+class checkedTodo(View):
+    def post(self, request, todo_id):
+
+        data = json.loads(request.body)
+
+        todo = TodoList.objects.get(id=todo_id)
+        todo.check = data['check']
+        todo.save()
 
         return JsonResponse({'result': 'success'}, status=200)
